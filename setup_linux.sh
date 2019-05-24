9cfa6a1da3111262aab11eb83c354fc8ff544375
broliang <18706703437@163.com>
2019-05-24 14:51:44 +0800

    Add git credential settings and check if package is already installed

--- setup_linux.sh
+++ setup_linux.sh
@@ -15,5 +15,21 @@ apps_to_install=(
 )
 
 for app in "${apps_to_install[@]}"; do
-	apt install -y ${app}
+	PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $app | grep "install ok installed")
+	if [ "" == "$PKG_OK" ]; then
+		echo -e "${app} not installed, will install it\n"
+		apt install -y ${app}
+	else
+		echo -e "${app} already installed, will skip it"
+	fi
 done
+
+# git credential settings
+read -rn 1 -p "Save git credentials to an unencrypted file to avoid writing? [y/N] " git_save_credential
+if [[ $git_save_credential =~ ^([Yy])$ ]]; then
+	echo -e "[credential]\nhelper = store\n" > ~/.gitconfig.local
+	echo -e "\nWill save git credentials to an unencrypted file"
+else
+	echo -e "[credential]\nhelper = cache --timeout 7200\n" > ~/.gitconfig.local
+	echo -e "\nWill cache credentails for two hours"
+fi
