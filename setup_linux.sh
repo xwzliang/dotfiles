f45bcb51fb9c88a342d5cd36402ac512557f4468
broliang <18706703437@163.com>
2019-05-29 22:04:52 +0800

    Modify setup_linux, only prompt to user if gitconfig local doesn't already exist

--- setup_linux.sh
+++ setup_linux.sh
@@ -26,11 +26,14 @@ for app in "${apps_to_install[@]}"; do
 done
 
 # git credential settings
-read -rn 1 -p "Save git credentials to an unencrypted file to avoid writing? [y/N] " git_save_credential
-if [[ $git_save_credential =~ ^([Yy])$ ]]; then
-	echo -e "[credential]\nhelper = store\n" > ~/.gitconfig.local
-	echo -e "\nWill save git credentials to an unencrypted file"
-else
-	echo -e "[credential]\nhelper = cache --timeout 7200\n" > ~/.gitconfig.local
-	echo -e "\nWill cache credentails for two hours"
+git_config_local_file=~/.gitconfig.local
+if [ ! -f "$git_config_local_file" ]; then
+	read -rn 1 -p "Save git credentials to an unencrypted file to avoid writing? [y/N] " git_save_credential
+	if [[ $git_save_credential =~ ^([Yy])$ ]]; then
+		echo -e "[credential]\nhelper = store\n" > $git_config_local_file
+		echo -e "\nWill save git credentials to an unencrypted file"
+	else
+		echo -e "[credential]\nhelper = cache --timeout 7200\n" > $git_config_local_file
+		echo -e "\nWill cache credentails for two hours"
+	fi
 fi
