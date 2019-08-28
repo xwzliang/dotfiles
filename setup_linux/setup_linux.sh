d3132a4e8773454276402c5d9686ed5a3f4285a6
broliang <18706703437@163.com>
2019-08-28 00:41:01 +0000

    Modify setup_linux.sh: Move setting git credential to a seperate script for convenience

--- setup_linux/setup_linux.sh
+++ setup_linux/setup_linux.sh
@@ -96,15 +96,6 @@ for directory_git_local in "${!repos_git_clone[@]}"; do
 done
 
 
-# git credential settings
-git_config_local_file=$HOME/.gitconfig.local
-if [ ! -f "$git_config_local_file" ]; then
-	read -rn 1 -p "Save git credentials to an unencrypted file to avoid writing? [y/N] " git_save_credential
-	if [[ $git_save_credential =~ ^([Yy])$ ]]; then
-		echo -e "[credential]\nhelper = store\n" > $git_config_local_file
-		echo -e "\nWill save git credentials to an unencrypted file"
-	else
-		echo -e "[credential]\nhelper = cache --timeout 7200\n" > $git_config_local_file
-		echo -e "\nWill cache credentails for two hours"
-	fi
-fi
+# git credential settings, import setting_git_credential.sh
+dir="$(dirname "$0")"
+"$dir/setting_git_credential.sh"
