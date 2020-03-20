e107cf9c8b306512bd7a99f61b9a070eba5bdb46
broliang <18706703437@163.com>
2020-03-20 11:33:35 +0800

    Modify emacs.symlink and Rename emacs.d_symlink_macros: Make my_macros a package.

--- emacs.d_symlink_my_macros.el
+++ emacs.d_symlink_my_macros.el
@@ -1,3 +1,6 @@
+;;; my_macros.el --- my macros.
+
+;;; Code:
 (fset 'my_macro_org_copy_agenda_link_line_to_journal_checklist
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([3 111 108 27 106 97 3 12 13 13 27 13 escape 27 107 106] 0 "%d")) arg)))
 
@@ -9,3 +12,8 @@
 
 (fset 'my_macro_save_html_and_url
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ("xhelm-modexfind-file-other-window~/pipes/zotero/linked_urldGkPjp:xxfind-file-other-window~/pipes/zotero/amazon_book.htmldGk\\gyGjp:x\\xhelm-mode" 0 "%d")) arg)))
+
+
+
+(provide 'my_macros)
+;;; my_macros.el ends here
